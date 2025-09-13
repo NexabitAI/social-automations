332959812150ea44667836e1eafc923154248d5b
shamaskaramat <shams4195140@gmail.com>
2025-09-13 07:09:30 +0500

    modal auth updated

--- server/models/User.js
+++ server/models/User.js
@@ -1,32 +1,67 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-const userSchema = new mongoose.Schema({
-    username: String,
-    email: String,
-    password: String,
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+    // Platform-specific tokens for THIS user
     platformAuth: {
         facebook: {
             pageId: String,
-            pageName: String,
-            accessToken: String,
-            connectedAt: Date
+            accessToken: String, // Will be encrypted before saving
+            pageName: String
         },
         instagram: {
-            profileId: String,
-            accessToken: String,
-            connectedAt: Date
+            igUserId: String,
+            accessToken: String, // Encrypted
+            username: String
         },
         linkedin: {
-            profileId: String,
-            accessToken: String,
-            connectedAt: Date
+            accessToken: String, // Encrypted
+            refreshToken: String, // Encrypted - CRITICAL
+            memberId: String,
+            organizationId: String,
+            organizationName: String
         },
         twitter: {
-            profileId: String,
-            accessToken: String,
-            connectedAt: Date
+            // Twitter API v2 requires OAuth 2.0
+            accessToken: String, // Encrypted
+            refreshToken: String, // Encrypted
+            userId: String
         }
     }
-}, { timestamps: true });
+}, {
+    timestamps: true // Adds createdAt, updatedAt
+});
 
-module.exports = mongoose.model('User', userSchema);
+// Encrypt password before saving
+UserSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    next();
+});
+
+// Compare password for login
+UserSchema.methods.comparePassword = async function (candidatePassword) {
+    return await bcrypt.compare(candidatePassword, this.password);
+};
+
+// TODO: Add pre-save hooks to encrypt platformAuth tokens (using mongoose encryption or 'crypto')
+
+module.exports = mongoose.model('User', UserSchema);
